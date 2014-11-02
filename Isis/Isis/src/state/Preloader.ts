01a3d4ef6351cbbaef31fdcde36cbf7c9123ecaa
Christian Broomfield <christian.broomfield@posteo.de>
2014-11-02 21:37:51 +0100

    Rem: Duplicate assets.

--- Isis/Isis/src/state/Preloader.ts
+++ Isis/Isis/src/state/Preloader.ts
@@ -11,11 +11,11 @@
 
         loadAssets() {
             this.load.tilemap("oryx_tilemap", "assets/tilemaps/maps/oryx_test.json", null, Phaser.Tilemap.TILED_JSON);
-            this.load.image("oryx_creatures", "assets/tilemaps/oryx_creatures.png");
-            this.load.image("oryx_items", "assets/tilemaps/oryx_items.png");
-            this.load.image("oryx_tiles", "assets/tilemaps/oryx_tiles.png");
-            this.load.image("oryx_world", "assets/tilemaps/oryx_world.png");
-            this.load.image("oryx_world2", "assets/tilemaps/oryx_world2.png");
+            this.load.image("oryx_creatures", "assets/tilemaps/tiles/oryx_creatures.png");
+            this.load.image("oryx_items", "assets/tilemaps/tiles/oryx_items.png");
+            this.load.image("oryx_tiles", "assets/tilemaps/tiles/oryx_tiles.png");
+            this.load.image("oryx_world", "assets/tilemaps/tiles/oryx_world.png");
+            this.load.image("oryx_world2", "assets/tilemaps/tiles/oryx_world2.png");
         }
 
         create() {
